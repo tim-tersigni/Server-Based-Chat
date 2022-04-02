1ee1be7fe45b904c44010851160bcebeeb5eea1a
Tim <tim.tersigni@gmail.com>
2022-04-02 17:23:56 -0500

    Change subscriber id to uuid4, refactor server code

--- populate_subscribers.py
+++ populate_subscribers.py
@@ -7,6 +7,6 @@ with open('subscribers.data', 'w') as f:
     num_clients = int(input("Generate how many subscribers?\n"))
     for x in range(num_clients):
         id = random.randint(1111, 9999)
-        key = uuid.uuid1()
+        key = uuid.uuid4()
         f.write("{},{}\n".format(id, key))
 f.close()
--- server.py
+++ server.py
@@ -1,66 +1,98 @@
 import logging
 import socket
 import uuid
+import hashlib
 import secrets
+from collections import namedtuple
 
-# Set to logging.WARNING to remove info / debug output
-logging.basicConfig(level=logging.INFO)
+SUBSCRIBERS = loadSubscribers('subscribers.data')
+S_UDP_SOCKET = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# Load list of subscribers from subscribers.data
-subscriber_file = open('subscribers.data', 'r')
-subscribers = []
-for line in subscriber_file:
-    split_line = line.split(',')
-    sub_id = split_line[0]
-    subscribers.append(sub_id)
 
-# (c is for client, s is for socket in var names)
-buffer_size = 2048
-s_udp_ip = '127.0.0.1'
-s_udp_port = 12000
+def main():
+    # Set to logging.WARNING to remove info / debug output
+    logging.basicConfig(level=logging.INFO)
 
-# Create UDP datagram socket
-s_udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # (c is for client, s is for socket in var names)
+    buffer_size = 2048
+    s_udp_ip = '127.0.0.1'
+    s_udp_port = 12000
 
-# Bind UDP socket to address and ip
-s_udp_socket.bind((s_udp_ip, s_udp_port))
+    # Bind UDP socket to address and ip
+    S_UDP_SOCKET.bind((s_udp_ip, s_udp_port))
 
-# UDP Server Loop
-logging.info('UDP server listening...')
-while(True):
-    # Bytes received by the socket are formatted in a length 2 tuple:
-    # message, address
-    bytes_recv = s_udp_socket.recvfrom(buffer_size)
+    # UDP Server Loop
+    logging.info('UDP server listening...')
+    while(True):
+        # Bytes received by the socket are formatted in a length 2 tuple:
+        # message, address
+        bytes_recv = S_UDP_SOCKET.recvfrom(buffer_size)
 
-    c_message = bytes_recv[0].decode("utf-8")
-    c_ip = bytes_recv[1]
-    logging.info("Client message: \"{} \"".format(c_message))
-    logging.info("Client IP, port: {}".format(c_ip))
+        c_message = bytes_recv[0].decode("utf-8")
+        c_address_port = bytes_recv[1]
+        logging.info("Client message: \"{} \"".format(c_message))
+        logging.info("Client IP, port: {}".format(c_address_port))
 
-    # Is the message a protocol message? (a command for the server)
-    # SYNTAX OF PROTOCOL MESSAGES: !PROTOCOL ARG1 ARG2 ARGn...
-    if c_message[0] == '!':
-        c_message = c_message[1:]
-        protocol_split = c_message.split()
-        protocol_type = protocol_split[0]
-        c_id = c_message.split()[1]
-        logging.info(
-            "Protocol message detected, type = {}".format(protocol_type))
+        # Is the message a protocol message? (a command for the server)
+        # SYNTAX OF PROTOCOL MESSAGES: !PROTOCOL ARG1 ARG2 ARGn...
+        if c_message[0] == '!':
+            c_message = c_message[1:]
+            protocol_split = c_message.split()
+            protocol_type = protocol_split[0]
+            c_id = c_message.split()[1]
+            logging.info(
+                "Protocol message detected, type = {}".format(protocol_type))
 
-        # HELLO message
-        if protocol_type == 'HELLO':
-            # Verify client id is on list of subscribers
-            if c_id in subscribers:
-                print("Client {} is a subscriber\n".format(c_id))
+            # !HELLO
+            if protocol_type == 'HELLO':
+                protocolHello(c_id, c_address_port)
 
-                # TODO Send challenge message to client with random key
-
+            # Not a recognized protocol
             else:
-                print("Client {} is not a subscriber\n".format(c_id))
-        # Not a recognized protocol
-        else:
-            print("ERROR, {} is not a protocol.\n".format(protocol_type))
+                print("ERROR, {} is not a protocol.\n".format(protocol_type))
 
-    # TODO: non-protocol messages
+        # TODO: non-protocol messages
+        else:
+            logging.info("Client message is not a protocol message.\n")
+
+
+def loadSubscribers(file_path):
+    # return list of namedtuples for subscribers in format (id, key)
+    subscriber_file = open(file_path, 'r')
+    subscribers = []
+    Sub = namedtuple('Sub', ['id', 'key'])
+    for line in subscriber_file:
+        split_line = line.split(',')
+        sub_id = split_line[0]
+        sub_key = split_line[1]
+        s = Sub(sub_id, sub_key)
+        subscribers.append(s)
+    return subscribers
+
+
+def getSubscriber(client_id):
+    for s in SUBSCRIBERS:
+        if SUBSCRIBERS[s].id == client_id:
+            return SUBSCRIBERS[s]
+    return None
+
+
+def protocolHello(client_id, client_address_port):
+    if getSubscriber(client_id) != None:
+        print("Client {} is a subscriber\n".format(client_id))
+
+        # retrieve client's key and concatenate a random uuid, then encrypt with MD5
+        key = getSubscriber(client_id)[1]
+        key_rand = key + uuid.uuid4()
+        key_rand_hash = hashlib.md5(str.encode(key_rand))
+
+        # challenge client with hash
+        msg = "!CHALLENGE {}".format(key_rand_hash)
+        S_UDP_SOCKET.sendto(str.encode(msg), client_address_port)
+
     else:
-        logging.info("Client message is not a protocol message.\n")
+        print("Client {} is not a subscriber\n".format(client_id))
+
+
+if __name__ == '__main__':
+    main()
